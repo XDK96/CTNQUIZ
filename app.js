f748f3947257c190dc44197e4a3891233922cd46
XDK96 <43788087+XDK96@users.noreply.github.com>
2024-07-09 00:47:10 +0200

    first test

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 "use strict";
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 // use port 8000 unless there exists a preconfigured port
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const WebSocket = require('ws');
+
+let clients = [];
+const app = express();
 var port = process.env.PORT || 8000;
 
-app.listen(port);
+// Statikus fájlok kiszolgálása
+app.use(express.static(path.join(__dirname)));
+
+// Kezdőoldal kiszolgálása
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'server.html'));
+});
+app.get('/client', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client.html'));
+});
+
+// HTTP szerver létrehozása
+const server = http.createServer(app);
+
+// WebSocket szerver létrehozása
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+  
+    // Add new client to the list
+    clients.push(ws);
+	
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+		var ct=JSON.parse(message);
+        // Broadcast the message to all clients except the sender
+        clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(ct));
+            }
+        });
+    });
+    ws.on('close', function close() {
+        console.log('A client disconnected');
+
+        // Remove the client from the list
+        clients = clients.filter(client => client !== ws);
+    });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
