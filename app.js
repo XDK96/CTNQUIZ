e95f12e6d83e90f70517bade41d14d3333345b14
XDK96 <43788087+XDK96@users.noreply.github.com>
2024-07-09 15:58:23 +0200

    first version

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const WebSocket = require('ws');
+const fs = require('fs');
 
 let clients = [];
 const app = express();
@@ -21,6 +22,36 @@ app.get('/client', (req, res) => {
   res.sendFile(path.join(__dirname, 'client.html'));
 });
 
+app.get('/names', (req, res) => {
+const namePath = path.join(__dirname, 'names.txt');
+
+  fs.readFile(namePath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(data);
+  });
+});
+
+app.get('/quiz', (req, res) => {
+const quizPath = path.join(__dirname, 'quiz.txt');
+
+  fs.readFile(quizPath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(data);
+  });
+});
+
 // HTTP szerver létrehozása
 const server = http.createServer(app);
 
@@ -29,15 +60,16 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('New client connected');
-  
     // Add new client to the list
     clients.push(ws);
 	
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
+		
 		var ct=JSON.parse(message);
         // Broadcast the message to all clients except the sender
         clients.forEach(client => {
+			//console.log(client);
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify(ct));
             }
